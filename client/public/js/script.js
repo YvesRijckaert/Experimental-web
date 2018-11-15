2c0623c832481486830a55dd0a10a9ee8e673d1f
Arthur <arthur.segaert@student.howest.be>
2018-11-15 16:21:33 +0100

    Image changes on sound

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,4 +1,5 @@
 {
+
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   let source;
 
@@ -10,73 +11,197 @@
   const stop = document.querySelector(`.stop`);
   const url = `assets/audio/loyal.mp3`;
 
-  let camera, scene, renderer;
-  let geometry, material, mesh;
-  let cubes;
+  setTimeout(() => document.body.classList.add('render'), 60);
 
-  const getData = () => {
-    source = audioCtx.createBufferSource();
-    source.connect(analyser);
-    fetch(url)
-      .then(response => {
-        return response.arrayBuffer();
-      })
-      .then(buffer => {
-        audioCtx.decodeAudioData(buffer, decodedData => {
-          source.buffer = decodedData;
-          source.connect(audioCtx.destination);
+  window.CanvasSlideshow = function (options) {
+
+    //  SCOPE
+    let that = this;
+
+    //  OPTIONS
+    options = options || {};
+    options.stageWidth = options.hasOwnProperty('stageWidth') ? options.stageWidth : 1920;
+    options.stageHeight = options.hasOwnProperty('stageHeight') ? options.stageHeight : 1080;
+    options.pixiSprites = options.hasOwnProperty('sprites') ? options.sprites : [];
+    options.centerSprites = options.hasOwnProperty('centerSprites') ? options.centerSprites : false;
+    options.autoPlay = options.hasOwnProperty('autoPlay') ? options.autoPlay : true;
+    options.autoPlaySpeed = options.hasOwnProperty('autoPlaySpeed') ? options.autoPlaySpeed : [10, 3];
+    options.fullScreen = options.hasOwnProperty('fullScreen') ? options.fullScreen : true;
+    options.displaceScale = options.hasOwnProperty('displaceScale') ? options.displaceScale : [200, 70];
+    options.displacementImage = options.hasOwnProperty('displacementImage') ? options.displacementImage : '';
+    options.navElement = options.hasOwnProperty('navElement') ? options.navElement : document.querySelectorAll('.scene-nav');
+    options.displaceAutoFit = options.hasOwnProperty('displaceAutoFit') ? options.displaceAutoFit : false;
+    options.wacky = options.hasOwnProperty('wacky') ? options.wacky : false;
+    options.interactive = options.hasOwnProperty('interactive') ? options.interactive : false;
+    options.interactionEvent = options.hasOwnProperty('interactionEvent') ? options.interactionEvent : '';
+    options.displaceScaleTo = (options.autoPlay === false) ? [0, 0] : [20, 20];
+    options.textColor = options.hasOwnProperty('textColor') ? options.textColor : '#fff';
+    options.displacementCenter = options.hasOwnProperty('displacementCenter') ? options.displacementCenter : false;
+    options.dispatchPointerOver = options.hasOwnProperty('dispatchPointerOver') ? options.dispatchPointerOver : false;
+
+
+
+    //  PIXI VARIABLES
+    let renderer = new PIXI.autoDetectRenderer(options.stageWidth, options.stageHeight, { transparent: true });
+    let stage = new PIXI.Container();
+    let slidesContainer = new PIXI.Container();
+    let displacementSprite = new PIXI.Sprite.fromImage(options.displacementImage);
+    let displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+
+    const getData = () => {
+      source = audioCtx.createBufferSource();
+      source.connect(analyser);
+      fetch(url)
+        .then(response => {
+          return response.arrayBuffer();
+        })
+        .then(buffer => {
+          audioCtx.decodeAudioData(buffer, decodedData => {
+            source.buffer = decodedData;
+            source.connect(audioCtx.destination);
+          });
         });
-      });
-  };
+    };
 
-  const animate = () => {
-    requestAnimationFrame(animate);
+    //  INITIALISE PIXI
+    const initPixi = () => {
 
-    analyser.getByteFrequencyData(dataArray);
+      // Add canvas to the HTML
+      document.body.appendChild(renderer.view);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+      // Add child container to the main container 
+      stage.addChild(slidesContainer);
 
-    const scale = dataArray[0] / 30;
-    mesh.scale.z = scale < 1 ? 1 : scale;
+      // Enable Interactions
+      stage.interactive = true;
 
-    renderer.render(scene, camera);
-  };
+      console.log(renderer.view.style);
 
-  const createScene = () => {
-    camera = new THREE.PerspectiveCamera(70, 500 / 500, 0.01, 10);
-    camera.position.z = 1;
+      // Fit renderer to the screen
+      if (options.fullScreen === true) {
+        renderer.view.style.objectFit = 'cover';
+        renderer.view.style.width = '50%';
+        renderer.view.style.height = '50%';
+        renderer.view.style.top = '50%';
+        renderer.view.style.left = '50%';
+        renderer.view.style.webkitTransform = 'translate( -50%, -50% ) scale(1.2)';
+        renderer.view.style.transform = 'translate( -50%, -50% ) scale(1.2)';
+      } else {
+        renderer.view.style.maxWidth = '100%';
+        renderer.view.style.top = '50%';
+        renderer.view.style.left = '50%';
+        renderer.view.style.webkitTransform = 'translate( -50%, -50% )';
+        renderer.view.style.transform = 'translate( -50%, -50% )';
+      }
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffffff);
 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    material = new THREE.MeshNormalMaterial();
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+      displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(500, 500);
-    document.body.appendChild(renderer.domElement);
 
-    animate();
-  };
+      // Set the filter to stage and set some default values for the animation
+      stage.filters = [displacementFilter];
 
-  const init = () => {
-    getData();
+      if (options.autoPlay === false) {
+        displacementFilter.scale.x = 0;
+        displacementFilter.scale.y = 0;
+      }
 
-    play.onclick = () => {
+      if (options.wacky === true) {
+
+        displacementSprite.anchor.set(0.5);
+        displacementSprite.x = renderer.width / 2;
+        displacementSprite.y = renderer.height / 2;
+      }
+
+      displacementSprite.scale.x = 2;
+      displacementSprite.scale.y = 2;
+
+      // PIXI tries to fit the filter bounding box to the renderer so we optionally bypass
+      displacementFilter.autoFit = options.displaceAutoFit;
+
+      stage.addChild(displacementSprite);
+
+    };
+
+    //  LOAD SLIDES TO CANVAS
+    const loadPixiSprites = (sprites) => {
+
+      let rSprites = options.sprites;
+      let rTexts = options.texts;
+
+      for (let i = 0; i < rSprites.length; i++) {
+
+        let texture = new PIXI.Texture.fromImage(sprites[i]);
+        let image = new PIXI.Sprite(texture);
+        if (rTexts) {
+          let richText = new PIXI.Text(rTexts[i], style);
+          image.addChild(richText);
+
+          richText.anchor.set(0.5);
+          richText.x = image.width / 2;
+          richText.y = image.height / 2;
+        }
+        if (options.centerSprites === true) {
+          image.anchor.set(0.5);
+          image.x = renderer.width / 2;
+          image.y = renderer.height / 2;
+        }
+        if (i !== 0) {
+          TweenMax.set(image, { alpha: 0 });
+        }
+        slidesContainer.addChild(image);
+      }
+    };
+
+    const init = () => {
+
+      let isRunning = true;
+
+      initPixi();
+      loadPixiSprites(options.pixiSprites);
+
       getData();
-      source.start(0);
-      play.disabled = true;
-    };
 
-    stop.onclick = () => {
-      source.stop(0);
-      play.disabled = false;
-    };
+      //renderen image
+      let render = new PIXI.ticker.Ticker();
+      render.autoStart = true;
+      render.add(function (delta) {
+        renderer.render(stage);
+      });
 
-    createScene();
+      //buttons
+      play.onclick = () => {
+        source.start(0);
+        play.disabled = true;
+        isRunning = true;
+      };
+
+      stop.onclick = () => {
+        source.stop(0);
+        play.disabled = false;
+        isRunning = false;
+      };
+
+      if (isRunning) {
+        animation();
+      }
+
+    }
+
+    const animation = () => {
+
+      analyser.getByteFrequencyData(dataArray);
+
+      let scaleX = dataArray[0];
+
+      requestAnimationFrame(animation);
+      if (scaleX != 0) {
+        scaleX = (scaleX - 90)
+        TweenMax.to(displacementFilter.scale, 1, { x: "+=" + Math.sin(scaleX) * 100 + "", y: "+=" + Math.cos(scaleX) * 50 + "" });
+      }
+    }
+
+    init();
   };
-  init();
-}
+}; 
