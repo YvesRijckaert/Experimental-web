2e4ef0e0877adeb8c3672a6085c9c710be466063
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-11-15 15:13:06 +0100

    Log in with spotify works, shows username

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
+import queryString from "query-string";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    };
+  }
+  componentDidMount() {
+    const parsed = queryString.parse(window.location.search);
+    const accessToken = parsed.access_token;
+    if (!accessToken) return;
+
+    fetch("https://api.spotify.com/v1/me", {
+      headers: { Authorization: "Bearer " + accessToken }
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          serverData: data,
+          user: data.display_name
+        })
+      );
+  }
+
   render() {
     return (
       <div className="app">
+        {this.state.user ? (
+          <p>Welcome, {this.state.user}</p>
+        ) : (
+          <button
+            onClick={() => {
+              window.location = window.location.href.includes("localhost")
+                ? "http://localhost:8888/login"
+                : "https://better-playlists-backend.herokuapp.com/login";
+            }}
+          >
+            Sign in with Spotify
+          </button>
+        )}
       </div>
     );
   }
