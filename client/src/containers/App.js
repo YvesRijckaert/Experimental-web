73b3030c99e72e49b4405a1bea92b042317b141a
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-11-15 15:47:31 +0100

    Shows playlists of user

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -23,13 +23,31 @@ class App extends Component {
           user: data.display_name
         })
       );
+
+    fetch("https://api.spotify.com/v1/me/playlists", {
+      headers: { Authorization: "Bearer " + accessToken }
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          playlists: data.items
+        })
+      );
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.user ? (
-          <p>Welcome, {this.state.user}</p>
+        {this.state.user && this.state.playlists ? (
+          <>
+            <h1>Welcome, {this.state.user}</h1>
+            <h2>Choose a playlist to create an artwork:</h2>
+            <ul>
+              {this.state.playlists.map(playlist => (
+                <li key={playlist.id}>{playlist.name}</li>
+              ))}
+            </ul>
+          </>
         ) : (
           <button
             onClick={() => {
