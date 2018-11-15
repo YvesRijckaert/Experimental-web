2fa7a07752b8ab0e12c27114a111416f022f11bf
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-11-15 15:21:39 +0100

    Server side landing page added

--- server/server.js
+++ server/server.js
@@ -1,45 +1,54 @@
-const express = require('express');
-const request = require('request');
-const querystring = require('querystring');
+const express = require("express");
+const request = require("request");
+const querystring = require("querystring");
 
 const app = express();
 
-const redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+const redirect_uri =
+  process.env.REDIRECT_URI || "http://localhost:8888/callback";
 
-app.get('/login', (req, res) => {
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
-      redirect_uri
-    }))
-})
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get('/callback', (req, res) => {
-  const code = req.query.code || null
+app.get("/login", (req, res) => {
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: "user-read-private user-read-email",
+        redirect_uri
+      })
+  );
+});
+
+app.get("/callback", (req, res) => {
+  const code = req.query.code || null;
   let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
+    url: "https://accounts.spotify.com/api/token",
     form: {
       code: code,
       redirect_uri,
-      grant_type: 'authorization_code'
+      grant_type: "authorization_code"
     },
     headers: {
-      'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-      ).toString('base64'))
+      Authorization:
+        "Basic " +
+        new Buffer(
+          process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+        ).toString("base64")
     },
     json: true
-  }
+  };
   request.post(authOptions, (error, response, body) => {
-    const access_token = body.access_token
-    const uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
-  })
-})
+    const access_token = body.access_token;
+    const uri = process.env.FRONTEND_URI || "http://localhost:3000";
+    res.redirect(uri + "?access_token=" + access_token);
+  });
+});
 
-const port = process.env.PORT || 8888
-app.listen(port)
+const port = process.env.PORT || 8888;
+app.listen(port);
