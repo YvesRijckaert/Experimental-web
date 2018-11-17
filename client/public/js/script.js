3e425ba7e4c69f6f833ffd3a41e637b4232d40b4
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-11-17 13:56:37 +0100

    added machine learning style transfer

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -13,7 +13,18 @@
   const stop = document.querySelector(`.stop`);
   const url = `../assets/audio/techno.mp3`;
 
+  const inputImg = document.getElementById('inputImg'); // The image we want to transfer
+  const styleA = document.getElementById('styleA'); // The div contrianer that holds new style image A
+
   const init = () => {
+    ml5.styleTransfer('../assets/models/techno')
+      .then(style1 => style1.transfer(inputImg))
+      .then(result => {
+        const newImage1 = new Image(250, 250);
+        newImage1.src = result.src;
+        styleA.appendChild(newImage1);
+      });
+
     getData();
 
     play.onclick = () => {
