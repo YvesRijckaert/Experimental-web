84cc057a264862a3138b97ff02ce9e8a92987d2a
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-11-28 12:51:51 +0100

    Playlists are now radio buttons

--- client/src/routes/StepOne.jsx
+++ client/src/routes/StepOne.jsx
@@ -6,12 +6,31 @@ const StepOne = ({ playlists }) => (
   <UserConsumer>
     {context => {
       return (
-        <section className="stepOne">
-          <h1>Welcome {context.userName}</h1>
-          <h2>Step01: Choose one of your playlists to create the artwork:</h2>
-          <ul>
+        <section className="step stepOne">
+          <h1 className="step-title">Welcome {context.userName}</h1>
+          <h2 className="step-subtitle">
+            Step01: Choose one of your playlists to create the artwork:
+          </h2>
+          <ul className="playlists">
             {playlists.map(playlist => (
-              <li key={playlist.id}>{playlist.name}</li>
+              <li key={playlist.id} className="playlists-item">
+                <label>
+                  <p className="playlists-item-name">{playlist.name}</p>
+                  <input
+                    name="playlist"
+                    type="radio"
+                    className="playlist-radiobutton"
+                    value={playlist.name}
+                  />
+                  <img
+                    src={playlist.images[0].url}
+                    className="playlists-item-image"
+                    width="640"
+                    height="640"
+                    alt={playlist.name}
+                  />
+                </label>
+              </li>
             ))}
           </ul>
           <Link to={`/stepTwo?access_token=${context.accessToken}`}>Next</Link>
