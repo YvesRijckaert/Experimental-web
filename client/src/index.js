c643cc6d36f6d610421429e9cd1bc9c62a4554dc
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-12-03 16:00:34 +0100

    font works

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import { BrowserRouter } from "react-router-dom";
+import WebfontLoader from "@dr-kobros/react-webfont-loader";
+
+import druktext from "./assets/fonts/druktext/druktext.css";
+
+const config = {
+  custom: {
+    families: ["druktext"],
+    urls: [druktext]
+  }
+};
 
 ReactDOM.render(
   <BrowserRouter>
+    <WebfontLoader config={config}>
       <App />
+    </WebfontLoader>
   </BrowserRouter>,
   document.getElementById(`root`)
 );
