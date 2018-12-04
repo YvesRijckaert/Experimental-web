b79a8fb357150265e0916ac375156356cadc02e9
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-12-04 14:32:28 +0100

    line animatons added

--- client/src/components/Lines.jsx
+++ client/src/components/Lines.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const style1 = {
   width: ".3rem",
   height: "100vh",
-  right: "20%",
+  right: "15%",
   top: "15%",
   background: "#373737",
   opacity: "1",
-  animation: `slidein 1s ease-out`,
+  animation: `slideInVertical 1s cubic-bezier(0.215, 0.610, 0.355, 1.000)`,
   transformOrigin: `${0}% ${100}% 0rem`,
   transform: "scaleY(1)"
 };
@@ -15,18 +15,32 @@ const style1 = {
 const style2 = {
   width: "100vw",
   height: ".3rem",
-  right: "20%",
+  right: "15%",
   top: "15%",
   background: "#373737",
   opacity: "1",
-  transformOrigin: `${50}% ${0}% 0rem`,
+  animation: `slideInHorizontal 1s cubic-bezier(0.215, 0.610, 0.355, 1.000)`,
+  transformOrigin: `${0}% ${0}% 0rem`,
   transform: "scaleY(1)"
 };
+
+const style3 = {
+    width: "100vw",
+    height: ".3rem",
+    right: "0%",
+    bottom: "15%",
+    background: "#373737",
+    opacity: "1",
+    animation: `slideInHorizontal 1s cubic-bezier(0.215, 0.610, 0.355, 1.000)`,
+    transformOrigin: `${0}% ${0}% 0rem`,
+    transform: "scaleY(1)"
+  };
 
 const Lines = ({ percentage }) => (
   <div className="decolines">
     <div className="decoline" style={style1} />
     <div className="decoline" style={style2} />
+    <div className="decoline" style={style3} />
   </div>
 );
 
