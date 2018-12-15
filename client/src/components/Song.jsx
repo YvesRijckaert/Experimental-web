3cf7782ed2512dad843db2722e4490f16b4fc1af
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-12-15 20:13:09 +0100

    selected song is saved in state

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
-const Song = ({title, artist, url, onClick}) => {
+const Song = ({title, artist, url, onClick, selectedSong}) => {
   const clickButton = e => {
     const value = e.currentTarget;
-    onClick(value);
   };
 
   return (
