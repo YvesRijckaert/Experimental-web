f4b412d762aea8332c5a5fa254d64134ffa6bc07
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-12-15 20:49:05 +0100

    arrow for active song added

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const Song = ({title, artist, url, onClick, selectedSong}) => {
   const clickButton = e => {
     const value = e.currentTarget;
+    onClick(value);
   };
 
   return (
-    <button className="canvas-option-song" data-song={url} onClick={clickButton}>
+    <button className={url === selectedSong ? 'canvas-option-song canvas-option-song-active' : 'canvas-option-song'} data-song={url} onClick={clickButton}>
       <span className="song-title">{title}</span>
       <span className="song-artist">{artist}</span>
     </button>
