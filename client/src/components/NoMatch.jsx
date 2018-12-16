4acf33dc07b9476677382605573dbf516022b1e4
Yves Rijckaert <yves.rijckaert@student.howest.be>
2018-12-16 16:05:18 +0100

    404 style

--- client/src/components/NoMatch.jsx
+++ client/src/components/NoMatch.jsx
@@ -5,11 +5,6 @@ import Line from "../components/Line";
 
 const NoMatch = () => (
   <React.Fragment>
-    <div className="decolines">
-      <Line pos="vertical" top="15" right="15" />
-      <Line pos="horizontal" top="15" right="15" />
-      <Line pos="horizontal" top="85" right="0" />
-    </div>
     <section className="main nomatch">
       <h2 className="subtitle login-subtitle">404, this page doesn't exist!</h2>
       <Link to="/">Take me home</Link>
